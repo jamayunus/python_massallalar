d63ba42b3742958e8ef688ce27772fae7fc111a3
Jamshid Yunusov <126382078+jamayunus@users.noreply.github.com>
2023-02-26 12:22:02 +0300

    yangi fayllar qo'shildi
    darslar yuklandi

---
+++ main (17).py
@@ -0,0 +1,73 @@
+import math
+
+x = float(input(' x ni kirit \n'))
+# y = float(input(' y ni kirit \n'))
+# z = float(input(' z ni kirit \n'))
+a = float(input(' a ni kirit \n'))
+b = float(input(' b ni kirit \n'))
+# t = float(input(' t ni kirit \n'))
+
+"""
+a = (2*math.cos(x-math.pi/6))/(1/2+math.pow(math.sin(y),2))
+b = 1+z**2/(3+math.pow(z,3)/5)
+print(a,"\n",b)
+"""
+"""
+j = math.fabs(math.pow(x,y/x)-math.pow(y/x,1/3))
+w = (y-x)*((y-z/(y-x))/(1+math.pow(y-x,2)))
+print(j,'\n',w)
+   """
+"""   
+S = 1+x+x**2/math.factorial(2)+math.pow(x,3)/math.factorial(3)+math.pow(x,4)/math.factorial(4)
+p = x*(math.sin(math.pow(x,3))+math.pow(math.cos(y),2))
+print(S,'\n',p)   
+""" 
+"""
+y = math.exp(-b*t)*math.sin(a*t+b)-math.sqrt(math.fabs(b*t+a))
+S = b*math.sin(a*t**2*math.cos(2*t))-1
+print(y,'\n',S)   
+   """
+"""
+W = math.sqrt(x**2+b)-b**2*math.pow(math.sin((x+a)/x),3) 
+y= math.pow(math.cos(math.pow(x,3)),2)-x/math.sqrt(a**2+b**2)
+print(W,'\n',y)
+   """
+"""   
+S = math.pow(x,3)*math.pow(math.tan((x+b)**2),2)+a/math.sqrt(x+b) 
+Q = (b*x**2-a)/a*x
+print(S,'\n',Q)
+"""
+R = x**2*(x+1)/b-math.sin(x+a)**2
+S = math.sqrt((x*b)/a)+math.pow(math.cos(math.pow((x+b),3)),2)
+print(R,'\n',S)
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
